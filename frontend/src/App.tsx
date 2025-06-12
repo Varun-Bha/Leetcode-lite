17a0c83981c2fa075b12cafab05eb0c19e374291
Varun <varunbha012@gmail.com>
2025-06-12 21:21:05 +0530

    committed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { initializeApp } from "firebase/app";
 import { Signin } from './components/Signin';
 import { getAuth, onAuthStateChanged  } from 'firebase/auth';
 import { use, useEffect } from 'react';
+import { useSetRecoilState, useRecoilValue } from 'recoil';
+import { userAtom } from './store/user';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBNi9KItAqxQTlKn8fNpxzvSRAHI-gPQUg",
@@ -20,20 +22,42 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
 function App() {
-useEffect(() => {
-  onAuthStateChanged(auth, function(user) {
-    if (user) {
-      console.log('This is the user: ', user)
-    } else {
-      // No user is signed in.
-      console.log('There is no logged in user');
-    }
-  });
-}, []);
+  const setUser = useSetRecoilState(userAtom); 
+  const user = useRecoilValue(userAtom);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, function(user) {
+      if (user && user.email) {
+        setUser({
+          loading: false,
+          user:{
+             email: user.email
+          }
+        })
+      } else {
+        setUser({
+          loading: false,
+           
+        })
+        // No user is signed in.
+        console.log('There is no logged in user');
+      }
+    });
+  }, []);
+
+  if(user.loading) {    
+    return <div>loading...</div>
+  }
+
+  if(!user) {    
+    return <div><Signin/></div>
+  }
+
+
   return (
     <>
       <div >
-        <Signin />
+        You are logged in as {user.user?.email}
       </div>
     </>
   )
