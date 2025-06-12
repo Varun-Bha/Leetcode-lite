548af64d4d02d89f18c27481dadca0f241f981c8
Varun <varunbha012@gmail.com>
2025-06-12 19:31:43 +0530

    committed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,7 @@
 
 import './App.css'
-import { initializeApp } from "firebase/app";
+
 import { Signin } from './components/Signin';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBNi9KItAqxQTlKn8fNpxzvSRAHI-gPQUg",
-  authDomain: "leetcode-lite-10145.firebaseapp.com",
-  projectId: "leetcode-lite-10145",
-  storageBucket: "leetcode-lite-10145.firebasestorage.app",
-  messagingSenderId: "472693523290",
-  appId: "1:472693523290:web:7f7c441e17d059359da963",
-  measurementId: "G-RL4PT9MCTS"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 function App() {
  
